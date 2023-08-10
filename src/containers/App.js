aa7f517b1d50982bbc16fa3a7613567c10ffcb5d
Nyasha Ivy Peace <nyashamuusha@gmail.com>
2023-08-10 22:11:26 +0200

    changes folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,22 @@
+import '../../src/App.css'
+import Header from '../components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+          Hello world
+        </div>
+        <Header/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React</a>
+    </div>
+  );
+}
+
+export default App;
