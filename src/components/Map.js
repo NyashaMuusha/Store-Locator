5a76343b64300bb9fc3676e01dd27e4d1aa695f9
Nyasha Ivy Peace <nyashamuusha@gmail.com>
2023-08-11 07:43:54 +0200

    added  map

---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../components/Map.css";
+
+const Map = () => {
+  return (
+    <div className="MapBox">
+      <img src="images/none.png" alt="No Store Selected" />
+    </div>
+  );
+};
+export default Map;
