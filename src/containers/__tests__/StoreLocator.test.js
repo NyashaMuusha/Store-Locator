6cb3051a7510052aff0a1f11b0ba6c1adf1cb21e
Nyasha Ivy Peace <nyashamuusha@gmail.com>
2023-08-12 18:31:06 +0200

    added tests

--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -2,6 +2,7 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'; // Use the correct adapter for your React version
 import StoreLocator from '../StoreLocator';
 import { act } from 'react-dom/test-utils'; 
+import axios from 'axios';
 
 configure({ adapter: new Adapter() });
 
@@ -11,6 +12,29 @@ let mountedStoreLocator;
 beforeEach(()=>{
     mountedStoreLocator = shallow(<StoreLocator />);
 });
+
+it('calls axios.get in #componentDidMount', ()=> {
+  return mountedStoreLocator.instance().componentDidMount().then(()=> {
+    expect(axios.get).toHaveBeenLastCalled();
+  })
+})
+it('calls axios.get with correct url', ()=> {
+  return mountedStoreLocator.instance().componentDidMount().then(()=> {
+    expect(axios.get).toHaveBeenLastCalledWith('http://localhost:3000/data/shops.json');
+  })
+})
+it('updates state with api fetchData', ()=> {
+  return mountedStoreLocator.instance().componentDidMount().then(()=> {
+    expect(mountedStoreLocator.state().toHaveProperty('shops',([
+      {
+        "location": 'test location',
+        "address": "test address"
+      }
+    ])));
+  })
+})
+
+
 
 it('renders without crashing', () => {
     let mountedStoreLocator = shallow(<StoreLocator />);
