216b61af0ed79793a7ef27fca932ba197937d786
Nyasha Ivy Peace <nyashamuusha@gmail.com>
2023-08-12 13:08:49 +0200

    small fixes

--- src/__tests__/mapChooser.test.js
+++ src/__tests__/mapChooser.test.js
@@ -1,14 +1,15 @@
+
 import mapChooser from '../mapChooser';
 
-describe('mapChooser', function(){
-  it('returns an image based on the input passed to it', function(){
-    let expected = 'portland.jpg';
-    let actual = mapChooser('portland');
-    expect(actual).toEqual(expected);
-  });
-  it('returns a default image if wrong input value is added',function(){
-    let expected = 'default.jpg';
-    let actual = mapChooser('default');
-    expect(actual).toEqual(expected);
-  })
-})
+describe("mapChooser", function(){
+    it("returns an image based on input given to it", function() {
+       let expected = "portland.jpg";
+       let actual = mapChooser("portland");
+       expect(actual).toEqual(expected);
+    });
+    it("returns an default image when no input is given", function() {
+        let expected = "default.jpg";
+        let actual = mapChooser("");
+        expect(actual).toEqual(expected);
+    });
+});
--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -4,27 +4,29 @@ import StoreLocator from '../StoreLocator';
 
 configure({ adapter: new Adapter() });
 
-describe('StoreLocator',() => {
-  let mountedStoreLocator;
-    beforeEach(()=>{
-      mountedStoreLocator = shallow(<StoreLocator/>);
-    })
-  it('renders without crashing', () => {
-  let mountedStoreLocator = shallow(<StoreLocator />);
-  expect(mountedStoreLocator.exists()).toBe(true);
+describe("StoreLocator", function(){
 
+let mountedStoreLocator;
+beforeEach(()=>{
+    mountedStoreLocator = shallow(<StoreLocator />);
 });
-  it('renders a header', () => {
-  const headers = mountedStoreLocator.find('Header');
-  expect(headers.length).toBe(1);
 
+it('renders without crashing', () => {
+    let mountedStoreLocator = shallow(<StoreLocator />);
 });
-it('renders two buttons', () => {
-  const buttons = mountedStoreLocator.find('Button');
-  expect(buttons.length).toBe(2);
+
+it('renders a header', () => {
+    const headers = mountedStoreLocator.find('Header');
+    expect(headers.length).toBe(1);
+});
+
+it('renders three buttons', () => {
+    const buttons = mountedStoreLocator.find('Button');
+    expect(buttons.length).toBe(3);
+});
+
+it('renders a map', () => {
+    const maps = mountedStoreLocator.find('Map');
+    expect(maps.length).toBe(1);
 })
-it('renders two a map', () => {
-  const map = mountedStoreLocator.find('Map');
-  expect(map.length).toBe(1);
-})
-})
+});
