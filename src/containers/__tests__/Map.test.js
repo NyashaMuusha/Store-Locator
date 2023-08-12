7b77ff22418e040916a1b2f95cd29a04de9d636a
Nyasha Ivy Peace <nyashamuusha@gmail.com>
2023-08-12 14:08:14 +0200

    added map and header tests

--- src/containers/__tests__/Map.test.js
+++ src/containers/__tests__/Map.test.js
@@ -1,25 +1,27 @@
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'; // Use the correct adapter for your React version
-import Map from '../../components/Map';
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Map from "../../components/Map";
 
 configure({ adapter: new Adapter() });
 
-describe('Map', () => {
+describe("Map", () => {
   let mountedMap;
-  beforeEach(()=> {
-    mountedMap = shallow(<Map/>);
-  })
-  it('renders without crashing', () => {
-  const mountedMap = shallow(<Map />);
-  expect(mountedMap.exists()).toBe(true);
-});
- it('renders without crashing', () => {
-  const img= mountedMap.find('img');
-  expect(img.length).toBe(1);
-})
- it('displays the none map when no params are given', () => {
-  const defaultMap = mountedMap.find('img [src="images/none.png"]');
-  expect(defaultMap.length).toBe(1);
- })
-})
 
+  beforeEach(() => {
+    mountedMap = shallow(<Map />);
+  });
+
+  it("renders without crashing", () => {
+    expect(mountedMap.exists()).toBe(true);
+  });
+
+  it("renders an image", () => {
+    const img = mountedMap.find("img");
+    expect(img.length).toBe(1);
+  });
+
+  it("displays the none map when no params are given", () => {
+    const defaultMap = mountedMap.find('img[src="images/none.png"]');
+    expect(defaultMap.length).toBe(1);
+  });
+});
